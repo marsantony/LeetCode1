7fe88add01500bd15ea49b1803a281b0df155e54
mars <marsantonymars1017@gmail.com>
2017-08-30 17:44:07 +0800

    8. String to Integer (atoi) 完成

---
+++ 8. String to Integer (atoi)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.String_to_Integer__atoi_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = new Solution().MyAtoi("2147483648");
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int MyAtoi(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return 0;
+
+            str = str.Trim();
+
+            int sign = 1, start = 0;long result = 0;
+            if(str[0] == '-')
+            {
+                sign = -1;
+                start = 1;
+            }
+            else if(str[0] == '+')
+            {
+                sign = 1;
+                start = 1;
+            }
+
+
+            for(int i = start; i < str.Length; i++)
+            {
+                if (!char.IsNumber(str[i]))
+                    return Convert.ToInt32(result * sign);
+                else
+                {
+                    result = result * 10 + (int)char.GetNumericValue(str[i]);
+
+                    if (sign == 1 && result >= Int32.MaxValue)
+                        return Int32.MaxValue;
+                    else if (sign == -1 && (result * sign) <= Int32.MinValue)
+                        return Int32.MinValue;
+                }
+            }
+
+            return Convert.ToInt32(result * sign);
+        }
+    }
+}
