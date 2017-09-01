bf3e10f914801b08869d28a058a16741534450a1
mars <marsantonymars1017@gmail.com>
2017-09-01 17:33:18 +0800

    9. Palindrome Number 完成

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool res = new Solution().IsPalindrome(12344321);
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            int rev = 0;
+            int t = x;
+            while (x > 0)
+            {
+                rev *= 10;
+                rev += x % 10;
+                x /= 10;
+            }
+
+            return rev == t;
+        }
+    }
+}
