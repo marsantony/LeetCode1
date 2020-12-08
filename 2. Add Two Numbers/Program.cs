72b5a1b65e2a1108fd08ea05677fb94080a876ab
MarsLiu <marsantonymars1017@gmail.com>
2020-12-08 15:33:43 +0800

    第二次重寫

--- 2. Add Two Numbers/Program.cs
+++ 2. Add Two Numbers/Program.cs
@@ -16,67 +16,48 @@ namespace _2.Add_Two_Numbers
             //ListNode res = new Solution().AddTwoNumbers(new ListNode(0) ,
             //   new ListNode(0));
 
-            ListNode res = new Solution().AddTwoNumbers(new ListNode(9),
-               new ListNode(1) { next = new ListNode(9)
-               {
-                   next = new ListNode(9)
-                   {
-                       next = new ListNode(9)
-                       {
-                           next = new ListNode(9)
-                           {
-                               next = new ListNode(9)
-
-                               {
-                                   next = new ListNode(9)
-                                   {
-                                       next = new ListNode(9)
-                                       {
-                                           next = new ListNode(9)
-                                           { next = new ListNode(9) }
-                                       }
-                                   }
-                               }
-                           }
-                       }
-                   }
-               } });
+            ListNode res = new Solution().AddTwoNumbers(
+                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+               );
             Console.ReadLine();
         }
     }
 
     /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int x) { val = x; }
- * }
- */
-
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int val=0, ListNode next=null) {
+     *         this.val = val;
+     *         this.next = next;
+     *     }
+     * }
+     */
     public class Solution
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            ListNode dummyHead = new ListNode(0);
-            ListNode p = l1, q = l2, curr = dummyHead;
-            int carry = 0;
-            while (p != null || q != null)
+            ListNode result = new ListNode();
+            ListNode node1 = l1, node2 = l2, curr = result;
+            int carry = 0, currVal = 0;
+            while (node1 != null || node2 != null)
             {
-                int x = p?.val ?? 0;
-                int y = q?.val ?? 0;
-                int sum = carry + x + y;
-                carry = sum / 10;
-                curr.next = new ListNode(sum % 10);
+                currVal = carry + (node1?.val ?? 0) + (node2?.val ?? 0);
+                carry = currVal / 10;
+                curr.next = new ListNode(currVal % 10);
+
+                node1 = node1?.next;
+                node2 = node2?.next;
                 curr = curr.next;
-                p = p?.next;
-                q = q?.next;
             }
             if (carry > 0)
             {
                 curr.next = new ListNode(carry);
             }
-            return dummyHead.next;
+
+            return result.next;
         }
     }
 
@@ -114,10 +95,16 @@ public class Solution
     }
 }
 */
+
     public class ListNode
     {
         public int val;
         public ListNode next;
-        public ListNode(int x) { val = x; }
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
+
 }
